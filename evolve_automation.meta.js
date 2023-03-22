d681c030b07b2d23dd8e08203aa6d2d00251ccfb
Volch <volk_pk@mail.ru>
2023-03-23 02:03:56 +0300

    Version up

--- evolve_automation.meta.js
+++ evolve_automation.meta.js
@@ -1,13 +1,14 @@
 // ==UserScript==
 // @name         Evolve
 // @namespace    http://tampermonkey.net/
-// @version      3.3.1.108.7
+// @version      3.3.1.108.8
 // @description  try to take over the world!
 // @downloadURL  https://gist.github.com/Vollch/b1a5eec305558a48b7f4575d317d7dd1/raw/evolve_automation.user.js
 // @updateURL    https://gist.github.com/Vollch/b1a5eec305558a48b7f4575d317d7dd1/raw/evolve_automation.meta.js
 // @author       Fafnir
 // @author       TMVictor
 // @author       Vollch
+// @author       schoeggu
 // @match        https://pmotschmann.github.io/Evolve/
 // @grant        none
 // @require      https://code.jquery.com/jquery-3.6.0.min.js
