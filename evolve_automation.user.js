c3225b184d786be3428c73dc061fa1e9d9781464
Kewne <67972089+kewne7768@users.noreply.github.com>
2024-07-02 23:44:24 +0200

    Fix resetTabHeight with showScriptSettings off

--- evolve_automation.user.js
+++ evolve_automation.user.js
@@ -14095,6 +14095,8 @@
 
     function resetTabHeight(tab) {
         let content = document.querySelector(`#script_${tab} .script-content`);
+        // May be possible if showScriptSettings is off.
+        if (!content) return;
         content.style.height = null;
         content.style.height = content.offsetHeight + "px";
     }
